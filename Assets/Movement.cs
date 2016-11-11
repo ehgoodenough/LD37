24e8c23840536d349429526214d29fe394b96a74
Andrew McPherson <andrewmcp333@gmail.com>
2016-11-10 21:29:54 -0800

    Added a character to move around.

---
+++ Assets/Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 700.0f;
+	private Rigidbody body;
+
+	void Start() {
+		this.body = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3(x, 0.0f, y);
+
+		this.body.AddForce(movement * speed * Time.deltaTime);
+	}
+}
