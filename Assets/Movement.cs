436819968c2b5b049483b5fcfe0dd2eb12c7eb53
Andrew McPherson <andrewmcp333@gmail.com>
2016-11-11 09:24:54 -0800

    Tried to restrict some of the movement.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-	public float speed = 700.0f;
+	public float speed = 25.0f;
 	private Rigidbody body;
 
 	void Start() {
@@ -11,11 +11,21 @@ public class Movement : MonoBehaviour {
 	}
 
 	void Update() {
-		float x = Input.GetAxis ("Horizontal");
-		float y = Input.GetAxis ("Vertical");
+		float x = Input.GetAxis ("Horizontal") * 25.0f;
+		float z = Input.GetAxis ("Vertical") * 25.0f;
+		float y = this.body.velocity.y;
 
-		Vector3 movement = new Vector3(x, 0.0f, y);
+		Vector3 movement = new Vector3(x, y, z);
+		this.body.velocity = movement;
 
-		this.body.AddForce(movement * speed * Time.deltaTime);
+		// float x = Input.GetAxis ("Horizontal");
+		// float z = Input.GetAxis ("Vertical");
+		//
+		// Vector3 movement = new Vector3(x, 0.0f, z);
+		// this.body.AddForce(movement * 10000);
+		//
+		// if(this.body.velocity.magnitude > this.speed) {
+		// 	this.body.velocity = this.body.velocity.normalized * this.speed;
+		// }
 	}
 }
