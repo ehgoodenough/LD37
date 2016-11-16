bbf9aeb11ac387b3a51477577285436652c22c0a
Paul M <paulmuren@gmail.com>
2016-11-15 16:56:23 -0800

    no message

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-	private float speed = 15.0f;
+    private float maxSpeed = 10.0f;
 	private Rigidbody body;
 
 	void Start() {
@@ -11,25 +11,28 @@ public class Movement : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-    float delta = Time.deltaTime / (1f / 60f);
-    
+        float delta = Time.deltaTime / (1f / 60f);
+        
 		Vector3 movement = new Vector3(0f, 0f, 0f);
-    
-    // movement.x = Input.GetAxis("Horizontal");
-    // movement.z = Input.GetAxis("Vertical");
-    movement.x = Input.GetAxisRaw("Horizontal");
-    movement.z = Input.GetAxisRaw("Vertical");
-    
-    movement = movement.normalized * this.speed * delta;
-    
-    movement = Camera.main.transform.TransformDirection(movement);
-    
-    this.body.velocity = movement; // does not preserve velocity.
-		// this.body.velocity += movement; // does not slow down yet.
-    // this.body.AddForce(movement); // feels unweildy and clunky.
-    
-		if(this.body.velocity.magnitude > this.speed) {
-			this.body.velocity = this.body.velocity.normalized * this.speed;
-		}
+        
+        // movement.x = Input.GetAxis("Horizontal");
+        // movement.z = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+
+        movement = movement.normalized;
+
+        // movement = Camera.main.transform.TransformDirection(movement);
+
+        //      this.body.velocity = movement; // does not preserve velocity.
+        //// this.body.velocity += movement; // does not slow down yet.
+        //      // this.body.AddForce(movement); // feels unweildy and clunky.
+
+        //if(this.body.velocity.magnitude > this.speed) {
+        //	this.body.velocity = this.body.velocity.normalized * this.speed;
+        //}
+
+        Vector3 targetVelocity = movement * maxSpeed;
+        this.body.velocity += (targetVelocity - this.body.velocity)/4 * delta;
 	}
 }
