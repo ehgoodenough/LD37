1911ad7811998a34bb2fc6854f0720ad33fa02a0
Andrew McPherson <mcphandr@amazon.com>
2016-11-26 15:03:19 -0800

    Added blocks that stack on blocks.

---
+++ Assets/Block.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+    
+    public float speed = 0.1f;
+    private Vector3 direction = Vector3.right;
+    
+    public void Initialize(int height) {
+        if(height % 2 == 0) {
+            this.transform.position = new Vector3(-10f, height + 0.5f, 0f);
+            this.direction = new Vector3(+1, 0, 0);
+        } else {
+            this.transform.position = new Vector3(0f, height + 0.5f, +10f);
+            this.direction = new Vector3(0, 0, -1);
+        }
+    }
+	
+	public void Move() {
+        // Normalize the delta.
+        float delta = Time.deltaTime / (1f / 60f);
+        
+        // Move the block.
+	    this.transform.Translate(this.direction * this.speed * delta);
+	}
+}
---
+++ Assets/Game.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Game : MonoBehaviour {
+    
+    public GameObject BlockPrefab;
+    
+    private GameObject block;
+    private List<GameObject> blocks;
+    
+    private int score = 0;
+    
+	void Start() {
+        GameObject block = Instantiate(BlockPrefab);
+        block.GetComponent<Block>().Initialize(this.score);
+        
+        blocks = new List<GameObject>();
+        blocks.Add(block);
+	}
+	
+	void Update() {
+	    if(Input.GetButtonDown("Action")) {
+            score += 1;
+            blocks.Add(InitializeBlock(score));
+        }
+        
+	    GetCurrentBlock().GetComponent<Block>().Move();
+	}
+    
+    // Instantiates a new block, and initializes
+    // it with some parameters of how it should spawn.
+    private GameObject InitializeBlock(int height) {
+        GameObject block = Instantiate(BlockPrefab);
+        block.GetComponent<Block>().Initialize(height);
+        return block;
+    }
+    
+    // Returns the block that we're actively
+    // stacking on top of all our other blocks.
+    // This is the block that is moving.
+    private GameObject GetCurrentBlock() {
+        return blocks[blocks.Count - 1];
+    }
+    
+    // Returns the block at the top of the stack.
+    private GameObject GetPreviousBlock() {
+        return blocks[blocks.Count - 2];
+    }
+}
