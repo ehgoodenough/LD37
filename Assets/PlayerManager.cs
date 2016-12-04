cf695b9c2d1e3e5f3fe8a8f6951bd326d87ba646
Andrew McPherson <mcphandr@amazon.com>
2016-12-04 15:56:21 -0800

    Drafted a clone of one-finger-death-punch.

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -3,41 +3,69 @@ using System.Collections;
 
 public class PlayerManager : MonoBehaviour {
 
-    private float maxSpeed = 10.0f;
-	private Rigidbody2D body;
+    public float attackRange = 3f;
 
-	void Start() {
-		body = GetComponent<Rigidbody2D>();
-	}
-
-	void FixedUpdate() {
+	void Update() {
         
-        // Normalize the delta.
-        float delta = Time.deltaTime / (1f / 60f);
+        // Initalize the attack as zero.
+        Vector2 attackingDirection = Vector2.zero;
         
-        ///////////////
-        // Position //
-        /////////////
+        // Listen for inputs to begn an attack. An
+        // attack can be either to the left or right.
+        if(Input.GetButtonDown("Left")) {
+            attackingDirection = Vector2.left;
+        } if(Input.GetButtonDown("Right")) {
+            attackingDirection = Vector2.right;
+        }
         
-        Vector2 movement = new Vector2(0f, 0f);
+        // If the attack is no longer zero, we
+        // begin an attack in that direction.
+        if(attackingDirection != Vector2.zero) {
+            // Send out a raycast in the direction of the attack. Only raycast within range. Only raycast against the enemy layer.
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, attackingDirection, attackRange, LayerMask.GetMask("Enemy"));
+            
+            // If the raycast hit an enemy...
+            if(hit.collider != null) {
+                    
+                // Move towards the enemy. We want to 
+                // teleport in front of the enemy. So we
+                // set our position to their position, then
+                // take one step backwards.
+                transform.position = hit.transform.position;
+                transform.Translate(attackingDirection * -1);
+                
+                // Hit the enemy. This should stun them
+                // so they no longer try to attack you.
+                hit.collider.gameObject.SendMessage("GetHit");
+                                
+                // Turn on the physics for the enemy.
+                hit.rigidbody.isKinematic = false;
+                
+                // Calculate the force and torque to push the enemy away.
+                Vector2 force = new Vector2(40f * attackingDirection.x, 15f);
+                float torque = -10f * attackingDirection.x;
+                
+                // Push the enemey away. They'll go flying through the air!
+                hit.rigidbody.AddForce(force, ForceMode2D.Impulse);
+                hit.rigidbody.AddTorque(torque, ForceMode2D.Impulse);
+            } else {
+                // TODO: If the player tries to attack an
+                // enemy that isn't yet in range, then they
+                // should "choke", and be stunned for a bit.
+                Debug.Log("You choked!");
+            }
+        }
         
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        movement = movement.normalized;
-
-        Vector2 targetVelocity = movement * maxSpeed;
-        body.velocity += (targetVelocity - body.velocity) / 4 * delta;
-        
-        ///////////////
-        // Rotation //
-        /////////////
-        
-        Vector3 direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg, Vector3.forward);
+        // The "hitzone" is the gray box rendered below the player. This is
+        // meant to show the players where their attacks are in range. If
+        // that public variable, `attackRange`, should be updated, we want
+        // our hitzone box to reflect that.
+        transform.FindChild("Hitzone").transform.localScale = new Vector2(attackRange * 2f, 0.5f);
 	}
     
-    void OnTriggerStay2D(Collider2D other) {
-        other.GetComponent<Renderer>().material.color = Color.red;
+    void OnTriggerEnter2D(Collider2D collider) {
+        // TODO: If the player is touched by an
+        // enemy, then they should die and restart.
+        Debug.Log("You died!");
     }
 }
