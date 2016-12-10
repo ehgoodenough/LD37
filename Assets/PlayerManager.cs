eda72b15d2bccc76ce4fbe6f77e6baa873df5238
Andrew McPherson <andrewmcp333@gmail.com>
2016-12-10 00:17:34 -0800

    temp

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -8,6 +8,8 @@ public class PlayerManager : MonoBehaviour {
     public float jumpSpeed = 20f;
     public float moveSpeed = 10f;
 
+    private GameObject held;
+
     void Start() {
         body = GetComponent<Rigidbody2D>();
     }
@@ -22,11 +24,36 @@ public class PlayerManager : MonoBehaviour {
         body.velocity += ((targetVelocity - body.velocity) / 4) * delta;
 
         // If player has hit the jump key...
-        if(Input.GetButton("Jump")) {
+        if(Input.GetKey("w") || Input.GetKey("up")) {
             // ...And if the player is standing on the ground...
-            if(Physics2D.Raycast(transform.position, Vector2.down, 0.25f, LayerMask.GetMask("Floor")).collider != null) {
-                // ...Then make them jump!
+            if(Physics2D.Raycast(transform.position, Vector2.down, 0.25f, LayerMask.GetMask("Dirt")).collider != null) {
+                // ...Then jump!
                 body.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            }
+        }
+
+        // If the player has hit the pickup key...
+        if(Input.GetKey("s") || Input.GetKey("down")) {
+            // ...And if the player isn't yet holding anything.
+            if(held == null) {
+                // ...And if the player is standing on something.
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.25f, LayerMask.GetMask("Dirt"));
+                if(hit.collider != null && hit.collider.gameObject.name != "Ground") {
+                    // ...Then pick it up!
+                    hit.rigidbody.isKinematic = true;
+                    hit.collider.enabled = false;
+
+                    hit.transform.parent = transform;
+                    hit.transform.localPosition = new Vector2(0, 3);
+
+                    held = hit.gameObject;
+                }
+            } else {
+                held.transform.parent = null;
+                held.rigidbody.isKinematic = true;
+                held.collider.enabled = false;
+                
+                held = null;
             }
         }
 	}
