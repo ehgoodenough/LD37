0a5d28ba596c566c8928fbc79a3bc7ec18e70812
Joseph Shiraef <Joseph Shiraef>
2016-12-10 21:24:28 -0800

    Scrolling dialogue text added

--- Assets/Dialogue.cs
+++ Assets/Dialogue.cs
@@ -1,16 +1,156 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Text))]
 public class Dialogue : MonoBehaviour {
 
+    private Text _textComponent;
+
+    public string[] DialogueStrings;
+
+    public float TimeBetweenCharacters = 0.05f;
+    public float CharacterRate = 0.5f;
+
+    public KeyCode DialogueInput = KeyCode.Return;
+
+    private bool _isStringBeingRevealed = false;
+    private bool _isDialoguePlaying = false;
+    private bool _isEndofDialogue = false;
+
+    public GameObject ContinueText;
+    public GameObject StopText;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        _textComponent = GetComponent<Text>();
+        _textComponent.text = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (!_isDialoguePlaying)
+            {
+                _isDialoguePlaying = true;
+                StartCoroutine(StartDialogue());
+            }
+
+        }
+
+      }
+
+        private IEnumerator StartDialogue()
+    {
+        int dialogueLength = DialogueStrings.Length;
+        int currentDialogueIndex = 0;
+
+        while(currentDialogueIndex < dialogueLength || !_isStringBeingRevealed)
+        {
+            if(!_isStringBeingRevealed)
+            {
+                _isStringBeingRevealed = true;
+                StartCoroutine(DisplayString(DialogueStrings[currentDialogueIndex++]));
+
+                if(currentDialogueIndex >= dialogueLength)
+                {
+                    _isEndofDialogue = true;
+                }
+            }
+
+            yield return 0;
+
+        }
+
+        while(true)
+        {
+            if(Input.GetKeyDown(DialogueInput))
+            {
+                break;
+            }
+
+            yield return 0;
+
+        }
+
+        HideStuff();
+        _isEndofDialogue = false;
+        _isDialoguePlaying = false;
+
+    }
+
+    private IEnumerator DisplayString(string stringToDisplay)
+    {
+        int stringLength = stringToDisplay.Length;
+        int currentCharacterIndex = 0;
+
+        HideStuff();
+
+        _textComponent.text = "";
+
+        while (currentCharacterIndex < stringLength)
+        {
+            _textComponent.text += stringToDisplay[currentCharacterIndex];
+            currentCharacterIndex++;
+
+            if (currentCharacterIndex < stringLength)
+            {
+                if (Input.GetKey(DialogueInput))
+                {
+                    yield return new WaitForSeconds(TimeBetweenCharacters * CharacterRate);
+                }
+                else
+                {
+                    yield return new WaitForSeconds(TimeBetweenCharacters);
+                }
+            }
+
+            else
+            {
+                break;
+            }
+        }
+
+        ShowIcon();
+
+        while (true)
+        {
+            if (Input.GetKeyDown(DialogueInput))
+            {
+                break;
+            }
+
+            yield return 0;
+        }
+
+        HideStuff();
+
+        _isStringBeingRevealed = false;
+        _textComponent.text = "";
+    }
+
+        
+
+    private void HideStuff()
+    {
+        ContinueText.SetActive(false);
+        StopText.SetActive(false);
+    }
+
+    private void ShowIcon()
+    {
+        if (_isEndofDialogue)
+        {
+            StopText.SetActive(true);
+            return;
+        }
+
+        ContinueText.SetActive(true);
+    }
+
 	}
-}
+
